6f426bb7b9dadc6069d6f329c8e61b74c32f5a2d
Rey Parma <reyparma@uppertext.com>
2024-03-25 17:49:05 -0400

    refactor: add instructions for each platform

--- starter_app/hooks/post_gen.dart
+++ starter_app/hooks/post_gen.dart
@@ -20,7 +20,20 @@ Future<void> run(HookContext context) async {
   // Run `flutter packages get` after generation.
   await Process.run('flutter', ['packages', 'get']);
   progress.complete();
-  print("\n==============================");
-  print("TODO: Additional instructions for IOS and Android setup");
-  print("==============================\n");
+
+  print("\n===================================================================");
+  print("WEB Version:");
+  print("\$ cd $name/");
+  print("\$ flutter run -d chrome --web-port=3000 --web-renderer=html");
+  print("\nIOS Version:");
+  print("\$ cd $name/");
+  print("\n\$ open ios/Runner.xcworkspace");
+  print("(Edit the bundle identifier and signing information as needed. See https://github.com/reyparma/bricks/tree/main/starter_app for more details.) When done, exit XCode and type:");
+  print("\n\$ flutter build ios");
+  print("\$ flutter run -d <IOS SIMULATOR DEVICE ID>");
+  print("\nANDROID Version:");
+  print("\$ cd $name/");
+  print("\$ flutter build apk");
+  print("\$ flutter run -d <ANDROID SIMULATOR DEVICE ID>");
+  print("\n===================================================================\n");
 }
